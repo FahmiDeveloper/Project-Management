483fce3d1ccda8c4f5a471a5188e81a8d8daf222
hosinoxfehmi <coursangularfahmi@gmail.com>
2026-06-30 19:35:56 +0100

    add part of notification

--- src/main/webapp/environments/environment.development.ts
+++ src/main/webapp/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   VERSION: 'DEV',
   DEBUG_INFO_ENABLED: true,
+  vapidPublicKey: 'BCZbxt9X8tv87HiX39pZzKfGl9Q2JEmOgikS8lwasFa-tcW37Jh4zvFxRql8684uR10LVCi5vQLeBT1-D1nrNLg',
 };
--- src/main/webapp/environments/environment.ts
+++ src/main/webapp/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   VERSION: __VERSION__,
   DEBUG_INFO_ENABLED: false,
+  vapidPublicKey: 'BCZbxt9X8tv87HiX39pZzKfGl9Q2JEmOgikS8lwasFa-tcW37Jh4zvFxRql8684uR10LVCi5vQLeBT1-D1nrNLg',
 };
