aef4ef9d6b753682d7724b9e867f024b7d5ec3d2
hosinoxfehmi <71970237+FahmiDeveloper@users.noreply.github.com>
2026-07-02 16:16:24 +0100

    delete work of add notification

--- src/main/webapp/environments/environment.ts
+++ src/main/webapp/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   VERSION: __VERSION__,
   DEBUG_INFO_ENABLED: false,
-  vapidPublicKey: 'BCZbxt9X8tv87HiX39pZzKfGl9Q2JEmOgikS8lwasFa-tcW37Jh4zvFxRql8684uR10LVCi5vQLeBT1-D1nrNLg',
 };
