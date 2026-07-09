f7f5dc62076abfa924b68672dad7225b89fe22c4
hosinoxfehmi <71970237+FahmiDeveloper@users.noreply.github.com>
2026-07-09 19:36:40 +0100

    update home and login page

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -13,16 +13,24 @@ import { PushSubscriptionService } from '../../core/service-worker/push-subscrip
 import { PushNotificationService } from '../../core/service-worker/push-notification.service';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { SidenavComponent } from '../sidenav/sidenav.component';
+import { HeaderComponent } from 'app/core/header';
+import { BodyComponent } from 'app/body/body.component';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'jhi-main',
   templateUrl: './main.component.html',
   providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, FooterComponent, PageRibbonComponent, CommonModule],
+  imports: [CommonModule, SidenavComponent, HeaderComponent, BodyComponent],
 })
 export default class MainComponent implements OnInit {
   private readonly renderer: Renderer2;
-
   private readonly router = inject(Router);
   private readonly appPageTitleStrategy = inject(AppPageTitleStrategy);
   private readonly accountService = inject(AccountService);
@@ -31,26 +39,37 @@ export default class MainComponent implements OnInit {
   private readonly swService = inject(CustomSwService);
   private readonly pushSubscriptionService = inject(PushSubscriptionService);
   private readonly pushNotificationService = inject(PushNotificationService);
-  private readonly http = inject(HttpClient);
+  private readonly deviceService = inject(DeviceDetectorService);
 
-  notificationSent = false;
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+  isConnected = false;
+  isMobile = false;
 
   constructor() {
     this.renderer = this.rootRenderer.createRenderer(document.querySelector('html'), null);
   }
 
   ngOnInit(): void {
-    // try to log in automatically
-    this.accountService.identity().subscribe(account => {
-      this.swService.register();
-      this.pushSubscriptionService.subscribe();
+    this.isMobile = this.deviceService.isMobile();
+
+    // 1. Listen to the continuous authentication state stream
+    this.accountService.getAuthenticationState().subscribe(account => {
       if (account) {
+        this.isConnected = true;
+        this.swService.register();
+        this.pushSubscriptionService.subscribe();
         this.pushNotificationService.init();
+      } else {
+        this.isConnected = false;
       }
       // setInterval(() => {
       //   this.pushNotificationService.sendNotification();
       // }, 10000);
     });
+
+    // try to log in automatically
+    this.accountService.identity().subscribe();
 
     this.translateService.onLangChange.subscribe((langChangeEvent: LangChangeEvent) => {
       this.appPageTitleStrategy.updateTitle(this.router.routerState.snapshot);
@@ -59,21 +78,8 @@ export default class MainComponent implements OnInit {
     });
   }
 
-  testNotification(): void {
-    this.http
-      .post(SERVER_API_URL + 'api/push/send', {
-        title: 'Hello 👋',
-        body: 'This is a test notification from your app! Tap the expand button on the right to read the rest of this extra long message safely inside your status tray.',
-        icon: '/content/icons/icon-192x192.png',
-        image: 'https://outburst-rocket-provoke.ngrok-free.dev/content/icons/icon-192x192.png',
-        url: '/',
-      })
-      .subscribe({
-        next: () => {
-          this.notificationSent = true;
-          setTimeout(() => (this.notificationSent = false), 3000); // reset after 3s
-        },
-        error: err => console.error('Notification error:', err),
-      });
+  onToggleSideNav(data: SideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
   }
 }
